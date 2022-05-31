8bc554ca32e5c65ccc275f6db1e5bf03d5187b6c
aniket-rulez <65964493+aniket-rulez@users.noreply.github.com>
2022-06-01 00:32:04 +0530

    Removed clist from __main__

--- tle/__main__.py
+++ tle/__main__.py
@@ -112,7 +112,7 @@ def main():
     # on_ready event handler rather than an on_ready listener.
     @discord_common.on_ready_event_once(bot)
     async def init():
-        clist_api.cache()
+        #clist_api.cache()
         await cf_common.initialize(args.nodb)
         asyncio.create_task(discord_common.presence(bot))
 
